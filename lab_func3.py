c6da9047d347b26d5185e366345e1baa3e97dda4
atharva454 <43982796+atharva454@users.noreply.github.com>
2018-10-09 13:44:54 +0530

    Add files via upload

---
+++ lab_func3.py
@@ -0,0 +1,16 @@
+def ArmN(x):
+  sum=0
+  t=x 
+
+  while (t>0): 
+   d=t%10
+   sum+=d**3
+   t=t//10
+  if x==sum: 
+     print(t,"it is an armstrong number")
+  else:
+     print(t,"it is not an armstrong number")
+ 
+x=int(input(""))
+print(ArmN(x))
+
